9af8b02cf0a5f35ea35914883f440c890b7964a3
chubbysoap <agledbetter795@gmail.com>
2025-05-04 13:34:48 -0400

    First commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+organization := "edu.berkeley.cs"
+version := "1.0"
+name := "first"
+scalaVersion := "2.12.4"
---
+++ src/main/scala/one.scala
@@ -0,0 +1,11 @@
+package yourproject
+
+import chisel3._
+
+class YourModule extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(32.W))
+    val out = Output(UInt(32.W))
+  })
+  io.out := io.in
+}
